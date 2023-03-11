af5fd8affd5ccc07f203e35d3815d2bc0891a39b
kameiryousuke <kamei.r@i.softbank.jp>
2023-03-11 10:55:26 +0900

    変更

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -12,7 +12,7 @@ const Experience = () => {
 
       <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory mt-24 md:mt-28">
         {sampleExperience.map((experience) => (
-          <ExperienceCard id={experience.id} {...experience} key={experience.id} />
+          <ExperienceCard  {...experience} key={experience.id} />
         ))}
       </div>
     </motion.div>
