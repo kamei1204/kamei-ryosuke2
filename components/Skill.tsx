0ed812b20c755e3f7b2a066b660994cc37d6a303
kameiryousuke <kamei.r@i.softbank.jp>
2023-03-31 09:43:28 +0900

    完成

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -15,7 +15,7 @@ const Skill:React.FC<directionProps> = ({ directionLeft, skill }) => {
         <div className='group relative flex cursor-pointer'>
             <motion.img
             initial={{ 
-                x: directionLeft ? -150 : 150 ,
+                x: directionLeft ? -120 : 120 ,
                 opacity: 0,
             }}
             transition={{
@@ -26,7 +26,7 @@ const Skill:React.FC<directionProps> = ({ directionLeft, skill }) => {
             }}
             src={skill.img} alt="" 
             className='rounded-full border rounded-gray-500 object-cover w-16 h-16 md:w-20 md:h-20  filter group-hover:grayscale transition duration-300 ease-in-out'/>
-            <div className='absolute opacity-0 group-hover:opacity-80 group-hover:bg-white transition duration-300 ease-in-out h-16 w-16 md:w-24 md:h-24 rounded-full'>
+            <div className='absolute opacity-0 group-hover:opacity-80 group-hover:bg-white transition duration-300 ease-in-out h-16 w-16 md:w-20 md:h-20 rounded-full'>
                 <div className='flex items-center justify-center h-full'>
                     <p className='text-2xl font-bold text-black opacity-100'>{skill.level}</p>
                 </div>
